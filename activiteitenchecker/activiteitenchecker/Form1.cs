152b898731a2af93833b94049471d455afaf0274
Bob van Dalen <Bob_Dalen@live.nl>
2014-01-24 20:06:41 +0100

    Buttons Added

--- activiteitenchecker/activiteitenchecker/Form1.cs
+++ activiteitenchecker/activiteitenchecker/Form1.cs
@@ -19,6 +19,10 @@ namespace activiteitenchecker
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
 
         }
 
